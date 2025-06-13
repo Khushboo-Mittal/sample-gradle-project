70458db261a36b08257a7f9a4fdea517f714257d
Khushboo Mittal <1154khb@gmail.com>
2025-06-13 11:42:14 +0530

    update tests

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -7,10 +7,6 @@ class CalculatorTest {
 
     Calculator calc = new Calculator();
 
-    void testAdd() {
-        assertEquals(5, calc.add(2, 3));
-    }
-
     @Test
     void testSubtract() {
         assertEquals(1, calc.subtract(3, 2));
